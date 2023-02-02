41af7ecb3a05e56dda5b7c6ce71ba8fa390b7b21
gowtham <gowthamfs7@gmail.com>
2023-02-02 12:53:31 +0530

    first commit

---
+++ css/script.js
@@ -0,0 +1,22 @@
+let main = document.querySelector('.mainGrid');
+
+main.addEventListener('wheel', (e)=>{
+    // e.preventDefault();
+    main.scrollLeft += e.deltaY;
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
